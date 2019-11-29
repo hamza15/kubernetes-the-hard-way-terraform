1dfe2fb65891cc7e0685f94e2529a0b3bf5395f8
Sardar Hamza Naqi <sardan5@vt.edu>
2019-11-28 23:29:24 -0500

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "var_project" {
+    default = "kthw-260206"
+}
+
+variable "var_region" {
+    default = "us-east1"
+}
+
+variable "var_zone" {
+    default = "us-east1-b"
+}
+
+variable "kthw_subnet" {
+    default = "10.240.0.0/24"
+}
